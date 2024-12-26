730f2305a484ee8f8835291fa53ae3dc55bcb639
JenyaCherny <jenya.cherny@gmail.com>
2024-12-25 21:07:00 -0500

    Completed experiment section and commented it out

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
-
+"use strict" /*
+console.log(20 + 5);
+console.log(23 - 12);
+console.log(5 + "0");
+console.log(18 - 4 * 11);
+console.log(25 / 5);
+console.log(15 % 4);
+console.log(12 == 12);
+console.log(10 == "10");
+console.log(5 === "5");
+console.log(2 || 3);
+console.log(3 || 2);
+console.log(2 && 4);
+console.log(5 && 7);
+*/
